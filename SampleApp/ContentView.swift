371d4564b7e4c69c20c7d3d6020c19b1c64fe50a
Gabby Bautista <gabby.bautistaaz@yahoo.com>
2020-04-05 10:50:02 -0700

    Initial Commit

---
+++ SampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by Gabby Bautista on 4/5/20.
+//  Copyright © 2020 Gabby Bautista. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
